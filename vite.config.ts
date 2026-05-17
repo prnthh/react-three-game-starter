4e5b57edcaec81969cdf722002579c8da9a5be8b
prnth <pranith_h@yahoo.com.sg>
2026-05-18 04:04:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const defaultBasePath = process.env.NODE_ENV === 'production' ? './' : '/'
+const defaultBasePath = process.env.NODE_ENV === 'production' ? '/react-three-game-starter/' : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
